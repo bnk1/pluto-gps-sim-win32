0b0be0cee32514e6dd6449a03b88e86c17cd6a1f
Boaz <6236472+bnk1@users.noreply.github.com>
2026-03-26 04:23:13 +0200

    Update LedsOff.bat

--- pluto-gps-sim/LedsOff.bat
+++ pluto-gps-sim/LedsOff.bat
@@ -1,3 +1,3 @@
 @echo off
-plink -ssh root@192.168.2.1 -pw analog "echo none | tee /sys/class/leds/led0:green/trigger; echo 0 | tee /sys/class/leds/led0:green/brightness"
-pause
+plink -ssh -T -batch root@192.168.2.1 -pw analog -no-antispoof "echo none > /sys/class/leds/led0:green/trigger; echo 0 > /sys/class/leds/led0:green/brightness" >nul 2>&1
+rem exit
